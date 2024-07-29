42485e0148c7c78eb370c93a7bca27655bec9c8e
Janis <janispfister@gmail.com>
2024-07-29 11:22:43 +0200

    initial Commit

---
+++ BluetoothConnector/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Management;
+using System.Security.Cryptography;
+
+namespace DeviceEnumerator
+{
+    class Program
+    {
+        private static ManagementObject device;
+        static void Main(string[] args)
+        {
+            
+            FindDevice();
+            
+        }
+
+        static void FindDevice()
+        {
+            try
+            {
+                string query = $"SELECT * FROM Win32_PnPEntity WHERE Description LIKE '%Bluetooth%' AND Name LIKE '%SRS-XB20 Stereo%'";
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);                
+                foreach (ManagementObject device in searcher.Get())
+                {
+                    Console.WriteLine($"Device Name: {device["Name"]}, Device ID: {device["DeviceID"]}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+        
+    }
+}
